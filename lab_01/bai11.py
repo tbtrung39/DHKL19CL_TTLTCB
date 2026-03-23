c0b15e4df8dd5abd1532e309f3a5cdccb0d8cdf3
Nguyen Huy Han <nhhan.25174600018@sv.uneti.edu.vn>
2026-03-23 09:56:33 +0700

    update

--- lab_01/bai11.py
+++
@@ -1,9 +0,0 @@
-import math
-
-a = float(input("Nhập vận tốc ban đầu a: "))
-
-mau_so = math.log(5, 4)
-
-t = (a**4) / mau_so
-
-print(f"Thời gian để ô tô dừng hẳn là: {round(t, 2)} giây")
--- lab_01/bai9.py
+++
@@ -1,7 +0,0 @@
-import math
-
-x = float(input("Nhập x (x > 0, x != 1): "))
-
-f_x = math.log(x, 4) + math.log(2, x)
-
-print(f"Giá trị f({x}) = {round(f_x, 2)}")
